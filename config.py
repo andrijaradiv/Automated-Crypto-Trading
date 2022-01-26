ef6cb4e37851ae8e09f99e80b03db1d0baaaf243
andrijaradiv <50723531+andrijaradiv@users.noreply.github.com>
2022-01-26 15:26:18 -0500

    add config file

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = 'Enter Binance API Key' # Binance API key
+api_secret = 'Enter Binance API Secret' # Binance API Secret key
+taapi_api_key = 'Enter Taapi API Key' # Taapi API https://taapi.io/
+
+TICKER = 'BTC' # Enter crypto ticker to trade
+BUY_AMOUNT_PERCENT = 0.15 # Enter percent of bnb to trade with each order
